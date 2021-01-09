b5942fe73f8e164b8f899a90295bbcce3fda21c4
nicokant <niccolocantu@gmail.com>
2021-01-09 12:56:17 +0100

    add navigate button to popups

--- src/components/Cluster/index.jsx
+++ src/components/Cluster/index.jsx
@@ -1,33 +1,52 @@
-import { useEffect } from 'react';
-import L from 'leaflet';
-import 'leaflet.markercluster/dist/leaflet.markercluster';
-import 'leaflet.markercluster/dist/MarkerCluster.css';
-import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
-import { useMap } from 'react-leaflet';
+import React from 'react';
+import 'react-leaflet-markercluster/dist/styles.min.css';
+import 'leaflet/dist/leaflet.css';
+import { Marker, Popup } from 'react-leaflet';
 import { getIconForFeature } from '../../assets/icons';
-
-const mcg = L.markerClusterGroup();
+import MarkerClusterGroup from 'react-leaflet-markercluster';
+import { Button, Typography } from 'antd';
+import { useDispatch } from 'react-redux';
+import { directionsActions } from '../../store/directions/slice';
+import { searchActions } from '../../store/search/reducer';
+import { appActions } from '../../store/app/slice';
 
 const MarkerCluster = ({ markers }) => {
-  const map = useMap();
+  const dispatch = useDispatch();
 
-  useEffect(() => {
-    mcg.clearLayers();
-    markers.forEach(({ position, text, layer, wheelchair }) =>
-      L.marker(new L.LatLng(position[1], position[0]), {
-        icon: getIconForFeature(layer, wheelchair)
-      })
-        .addTo(mcg)
-        .bindPopup(text)
-    );
-
-    // optionally center the map around the markers
-    // map.fitBounds(mcg.getBounds());
-    // // add the marker cluster group to the map
-    map.addLayer(mcg);
-  }, [markers, map]);
-
-  return null;
+  return (
+    <MarkerClusterGroup key={markers.length}>
+      {markers.map(({ position, text, layer, wheelchair, id }) => (
+        <Marker
+          key={id}
+          position={[position[1], position[0]]}
+          icon={getIconForFeature(layer, wheelchair)}
+        >
+          <Popup>
+            <Typography.Title level={5}>{text}</Typography.Title>
+            <div>
+              <Button
+                type="primary"
+                style={{
+                  margin: 'auto',
+                  display: 'block'
+                }}
+                onClick={() => {
+                  dispatch(directionsActions.setEndInput(text));
+                  dispatch(
+                    directionsActions.setEnd([position[1], position[0]])
+                  );
+                  dispatch(searchActions.cleanSearch());
+                  dispatch(appActions.openDirections());
+                }}
+              >
+                Vai
+              </Button>
+            </div>
+          </Popup>
+        </Marker>
+      ))}
+    </MarkerClusterGroup>
+  );
 };
 
 export default MarkerCluster;
--- src/components/LayerRender/index.jsx
+++ src/components/LayerRender/index.jsx
@@ -1,27 +1,29 @@
 import { useSelector } from 'react-redux';
 import { getFeaturesSelector } from '../../store/layers/selectors';
-import React, { useMemo } from 'react';
-import { GeoJSON, LayerGroup } from 'react-leaflet';
+import React from 'react';
+// import { GeoJSON, LayerGroup } from 'react-leaflet';
 import MarkerCluster from '../Cluster';
 
 export default function LayerRender() {
-  const { features, points } = useSelector(getFeaturesSelector);
+  const { points } = useSelector(getFeaturesSelector);
 
-  const geojsonFeatures = useMemo(() => {
-    return features.map(feature => <GeoJSON key={feature.id} data={feature} />);
-  }, [features]);
+  // const geojsonFeatures = useMemo(() => {
+  //   return features.map(feature => <GeoJSON key={feature.id} data={feature} />);
+  // }, [features]);
 
   return (
     <>
       <MarkerCluster
         markers={points.map(point => ({
+          id: point.id,
           position: point.geometry.coordinates,
           layer: point.layer,
           wheelchair: point.properties.wheelchair,
-          text: point.properties.name ? point.properties.name : point.id
+          text: point.properties.name ? point.properties.name : point.id,
+          properties: point.properties
         }))}
       />
-      <LayerGroup>{geojsonFeatures}</LayerGroup>
+      {/* <LayerGroup>{geojsonFeatures}</LayerGroup> */}
     </>
   );
 }
