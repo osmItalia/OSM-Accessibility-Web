f02555aa1baadc067057bac1aab23837691030e3
nicokant <niccolocantu@gmail.com>
2021-01-22 12:59:16 +0100

    add donation button

--- src/constants.js
+++ src/constants.js
@@ -27,3 +27,6 @@ export const TRAVEL_MEAN = {
 
 export const NEWSLETTER_IFRAME_SRC =
   'https://www.webarea.services/form_give/index.php?pagina=form&action=viewForm&idform=474&idEnass=c8ffe9a587b126f152ed3d89a146b445';
+
+export const PAYPAL_DONATION_URL =
+  'https://www.paypal.com/donate/?business=ECAW27MH2UEPU&item_name=test&currency_code=EUR';
