ffdc8f927fc1f8fc5699fbc15b40b8eb096ecbb1
nicokant <niccolocantu@gmail.com>
2021-04-01 11:51:32 +0200

    use 16px as base font-size to avoid auto-zoom on mobile form #2

--- config-overrides.js
+++ config-overrides.js
@@ -9,14 +9,8 @@ module.exports = override(
   addLessLoader({
     javascriptEnabled: true,
     modifyVars: {
-      '@primary-color': '#00bbd8',
-      '@secondary-color': '#2370B8',
-      '@layout-sider-background': '#e6efee',
-      '@layout-header-background': '#37474f',
-      '@layout-header-color': '#fff',
-      '@layout-body-background': '#f7f7f7',
+      '@font-size-base': '16px',
       '@form-item-label-font-size': '17px',
-      '@page-header-back-color': '#fff',
       '@font-family': `Lato, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
         'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
         'Noto Color Emoji'`
